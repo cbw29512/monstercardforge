25a25afa195b07d0cc5402fc58367e3f399ba1bb
Chris Wilson <divclass01@gmail.com>
2026-07-04 15:52:51 -0400

    Set starter sample as default route

--- src/js/appState.js
+++ src/js/appState.js
@@ -1,33 +1,18 @@
 import { homebrewExample } from '../data/monsters.js';
 
+const copy = (value) => JSON.parse(JSON.stringify(value));
+
 export const state = {
-  tab: 'library',
+  tab: 'free',
   ruleset: '5e-2014',
   selectedId: 'goblin-2014',
   type: 'all',
-  homebrew: JSON.parse(JSON.stringify(homebrewExample)),
+  homebrew: copy(homebrewExample)
 };
 
-export function setTab(tab) {
-  state.tab = tab;
-}
-
-export function setRuleset(ruleset) {
-  state.ruleset = ruleset;
-}
-
-export function setType(type) {
-  state.type = type;
-}
-
-export function setSelectedMonster(id) {
-  state.selectedId = id;
-}
-
-export function updateHomebrew(values) {
-  state.homebrew = { ...state.homebrew, ...values };
-}
-
-export function resetHomebrew() {
-  state.homebrew = JSON.parse(JSON.stringify(homebrewExample));
-}
+export function setTab(value) { state.tab = value; }
+export function setRuleset(value) { state.ruleset = value; }
+export function setType(value) { state.type = value; }
+export function setSelectedMonster(value) { state.selectedId = value; }
+export function updateHomebrew(values) { state.homebrew = { ...state.homebrew, ...values }; }
+export function resetHomebrew() { state.homebrew = copy(homebrewExample); }
